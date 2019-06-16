490fe7979ab3a4ade6c53fd5a5a7bc8917ec0ce3
Brian K <>
2019-06-16 13:32:02 -0400

    worked on the analysis layer trying to correlate nitrate and cancer

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,7 @@ var wellSitesStyle = {
 };
 
 var wellPoints;
+var censusTracts;
 // END DEFINE GLOBAL VARIABLES
 // --------------------------------------------------------------------------
 
@@ -31,15 +32,24 @@ var wellPoints;
 // add the mapbox tiles to the map object
 map.addLayer(mapboxTiles);
 
+var exponent = 1;
+var cellSize = 3;
+var exponentInput = document.getElementById("exponentInput");
+var interpolateButton = document.getElementById("interpolate");
+var removeInterpolateButton = document.getElementById("removeInterpolate");
 
+var cancerRateButton = document.getElementById("cancerRate");
+var removeCancerButton = document.getElementById("removeCancer");
 
 var wellLayer = L.geoJSON(null, {
         pointToLayer: function (feature, latlng) {
             return L.circleMarker(latlng, wellSitesStyle);
         }
     }).addTo(map);
-var censusLayer = L.geoJSON().addTo(map);
+var censusLayer = L.geoJSON(null, {style:styleTracts}).addTo(map);
 var countiesLayer = L.geoJSON().addTo(map);
+var rates = L.geoJSON(null, {style:styleInterpolation});
+var cancerRates = L.geoJSON(null, {style:styleCancer});
 
 
 // addLayers(layers);
@@ -63,9 +73,42 @@ L.control.layers(null, overlayLayers).addTo(map);
 
 // --------------------------------------------------------------------------
 
+
+// ADD EVENT LISTENERS
+// --------------------------------------------------------------------------
+exponentInput.addEventListener("change", function(){
+    exponent = Number(exponentInput.value);
+});
+
+interpolateButton.addEventListener("click", function(){
+    createInterpolation(wellPoints);
+});
+
+removeInterpolateButton.addEventListener("click",function(){
+   map.removeLayer(rates);
+});
+
+cancerRateButton.addEventListener("click", function(){
+    createCancerLayer(censusTracts);
+});
+
+removeCancerButton.addEventListener("click",function(){
+    map.removeLayer(cancerRates);
+});
+
+map.on('click', function(e){
+    var coord = e.latlng;
+    var lat = coord.lat;
+    var lng = coord.lng;
+    console.log("You clicked the map at latitude: " + lat + " and longitude: " + lng);
+});
+
+// --------------------------------------------------------------------------
+
+
 // Define Functions
 function addCounties(){
-    $.ajax("assets/data/WICounties.topojson", {
+    $.ajax("assets/data/WICounties.geojson", {
         dataType: "json",
         success: createCountyLayer
     });
@@ -78,7 +121,7 @@ function createCountyLayer(response, status, jqXHRobject){
 
 // Rename all functions and variables
 function addCensusTracts(){
-    $.ajax("assets/data/WICensusTracts.topojson", {
+    $.ajax("assets/data/WICensusTracts.geojson", {
         dataType: "json",
         success: createCensusLayer
     });
@@ -86,14 +129,18 @@ function addCensusTracts(){
 
 function createCensusLayer(response, status, jqXHRobject){
 
+    censusTracts = response;
+
     censusLayer.addData(response)
     censusLayer.bringToBack(map);
+
+    cancerRateButton.disabled = false;
 };
 
 
 // Define Functions
 function addWellSites(){
-    $.ajax("assets/data/WellSites.topojson", {
+    $.ajax("assets/data/WellSites.geojson", {
         dataType: "json",
         success: createWellSitesLayer
     });
@@ -109,27 +156,36 @@ function createWellSitesLayer(response, status, jqXHRobject){
     });
     wellLayer.bringToFront(map);
 
-    createInterpolation(wellPoints);
+    // Make the interpolation button active
+    interpolateButton.disabled = false;
+
+    // createInterpolation(wellPoints);
 };
 
 
 function createInterpolation(wellPoints){
-    console.log(wellPoints);
 
-    var exponent = 1;
+    map.removeLayer(rates);
+    rates.clearLayers();
+
+    // var options = {gridType: 'hex', property: 'nitr_ran', units: 'miles', weight: exponent};
+    // var grid = turf.interpolate(wellPoints, 5, options);
+
     var options = {gridType: 'hex', property: 'nitr_ran', units: 'miles', weight: exponent};
-    var grid = turf.interpolate(wellPoints, 5, options);
+    var grid = turf.interpolate(wellPoints, cellSize, options);
 
     console.log(grid);
 
-    var rates = L.geoJSON(grid, {style:style});
+    rates.addData(grid);
 
     rates.addTo(map);
-    rates.bringToBack(map);
+
+    // collectPoints(grid);
+    // rates.bringToBack(map);
 };
 
 
-function getColor(d) {
+function getInterpolationColor(d) {
     return d > 5 ? '#993404' :
             d > 4  ? '#d95f0e' :
                 d > 3  ? '#fe9929' :
@@ -137,9 +193,9 @@ function getColor(d) {
                         '#ffffd4';
 }
 
-function style(feature) {
+function styleInterpolation(feature) {
     return {
-        fillColor: getColor(feature.properties.nitr_ran),
+        fillColor: getInterpolationColor(feature.properties.nitr_ran),
         weight: 2,
         opacity: 1,
         color: 'white',
@@ -147,3 +203,98 @@ function style(feature) {
         fillOpacity: 0.7
     };
 }
+
+function getTractsColor(d) {
+    return d > .8 ? '#993404' :
+            d > .6  ? '#d95f0e' :
+                d > .4  ? '#fe9929' :
+                    d > .2  ? '#fed98e' :
+                        '#ffffd4';
+}
+
+function styleTracts(feature) {
+    return {
+        fillColor: getTractsColor(feature.properties.canrate),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+
+
+function getCancerColor(d) {
+    return d > .8 ? '#993404' :
+        d > .6  ? '#d95f0e' :
+            d > .4  ? '#fe9929' :
+                d > .2  ? '#fed98e' :
+                    '#ffffd4';
+}
+
+function styleCancer(feature) {
+    return {
+        fillColor: getCancerColor(feature.properties.canrate),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+
+function createCancerLayer(censusTracts) {
+
+    testCancerInterpolate(censusTracts);
+    // map.removeLayer(cancerRates);
+    // cancerRates.clearLayers();
+    //
+    // var bbox = turf.bbox(wellPoints);
+    // console.log(bbox);
+    // var cellSide = 10;
+    // var options = {units: 'miles', properties:{canrate: 0}};
+    // var hexGrid = turf.hexGrid(bbox, cellSide, options);
+    //
+    // console.log(hexGrid);
+    //
+    // cancerRates.addData(hexGrid);
+    //
+    // cancerRates.addTo(map);
+};
+
+function collectPoints(grid){
+    var collected = turf.collect(censusTracts, grid, 'nitr_ran', 'canrate');
+    console.log(collected);
+};
+
+function testCancerInterpolate(censusTracts){
+    console.log("Test Intersect -----------------------------");
+
+    var tractCentroids = [];
+    turf.featureEach(censusTracts, function(currentFeature, featureIndex){
+        var centroid = turf.centroid(currentFeature);
+        centroid.properties = {canrate:currentFeature.properties.canrate};
+        console.log(centroid, centroid.properties.canrate);
+        tractCentroids.push(centroid);
+    });
+
+    console.log(tractCentroids);
+
+    var tractPoints = turf.featureCollection(tractCentroids);
+
+
+    var options = {gridType: 'hex', property: 'canrate', units: 'miles', weight: exponent};
+    var grid = turf.interpolate(tractPoints, cellSize, options);
+    // Interpolate the points to a grid
+
+    console.log(grid);
+
+    var canRates = L.geoJSON(grid, {style:styleCancer});
+
+    canRates.addTo(map);
+
+    console.log("Test Intersect End -------------------------");
+
+};
