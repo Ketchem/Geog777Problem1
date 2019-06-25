1500b084b7d5bf1ca1947e6e674ca6d6e79c9a1e
Brian K <>
2019-06-25 15:18:56 -0400

    Redesigned layout to be full screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,22 +3,20 @@
 // mapbox access token
 var accessToken = 'pk.eyJ1Ijoia2V0Y2hlbTIiLCJhIjoiY2pjYzQ5ZmFpMGJnbTM0bW01ZjE5Z2RiaiJ9.phQGyL1FqTJ-UlQuD_UFpg';
 
-//  mapbox tiles
-// https://api.mapbox.com/styles/v1/ketchem2/cjxaofh553ssx1cpdh6nkjqo3.html?title=true&access_token=pk.eyJ1Ijoia2V0Y2hlbTIiLCJhIjoiY2pjYzQ5ZmFpMGJnbTM0bW01ZjE5Z2RiaiJ9.phQGyL1FqTJ-UlQuD_UFpg#6.1/44.622012/-89.976147/0
-// https://api.mapbox.com/styles/v1/ketchem2/cjxaofh553ssx1cpdh6nkjqo3.html?fresh=true&title=true&access_token=pk.eyJ1Ijoia2V0Y2hlbTIiLCJhIjoiY2pjYzQ5ZmFpMGJnbTM0bW01ZjE5Z2RiaiJ9.phQGyL1FqTJ-UlQuD_UFpg#6.1/44.622012/-89.976147/0
-// var mapboxTiles = L.tileLayer('https://api.mapbox.com/styles/v1/ketchem2/cjxaofh553ssx1cpdh6nkjqo3.html?access_token=' + accessToken + '#6.1/44.622012/-89.976147/0', {
-//     attribution: '<a href="https://www.mapbox.com/feedback/">Mapbox</a> <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// });
-
-var mapboxTiles = L.tileLayer('https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=' + accessToken, {
+var mapboxTiles = L.tileLayer('https://api.mapbox.com/v4/mapbox.dark/{z}/{x}/{y}.png?access_token=' + accessToken, {
     attribution: '<a href="https://www.mapbox.com/feedback/">Mapbox</a> <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
+
+// var mapboxTiles = L.tileLayer('https://api.mapbox.com/styles/v1/ketchem2/cjxaofh553ssx1cpdh6nkjqo3.html?fresh=true&title=true&access_token=pk.eyJ1Ijoia2V0Y2hlbTIiLCJhIjoiY2pjYzQ5ZmFpMGJnbTM0bW01ZjE5Z2RiaiJ9.phQGyL1FqTJ-UlQuD_UFpg', {
+//     attribution: '<a href="https://www.mapbox.com/feedback/">Mapbox</a> <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// });
 
 // create the map object and set the center and zoom
 var map = L.map('map', {
     center: [44.75, -89.65],
-    zoom: 7
+    zoom: 7.2
 });
+
 
 // Layer Styles
 var wellSitesStyle = {
@@ -46,7 +44,7 @@ var wellLayer = L.geoJSON(null, {
     pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, wellSitesStyle);
     }
-}).addTo(map);
+}).addTo(map);;
 var censusLayer = L.geoJSON(null, {style:styleTracts}).addTo(map);
 var countiesLayer = L.geoJSON(null, {style:countyStyle}).addTo(map);
 var nitrateLayer = L.geoJSON(null, {style:styleInterpolation});
@@ -96,20 +94,19 @@ addWellSites();
 addCensusTracts();
 addCounties();
 
-
-var overlayLayers = {
-    "Well Sites": wellLayer,
-    "Census Tracts" : censusLayer,
-    "Counties" : countiesLayer
-}
 // END BUILD MAP
 // --------------------------------------------------------------------------
 
 
 // ADD LAYER CONTROLS
 // --------------------------------------------------------------------------
-L.control.layers(null, overlayLayers).addTo(map);
+var legendLayers = {
+    "Well Sites": wellLayer,
+    "Census Tracts" : censusLayer,
+    "Counties" : countiesLayer
+};
 
+L.control.layers(null, legendLayers).addTo(map);
 // --------------------------------------------------------------------------
 
 
